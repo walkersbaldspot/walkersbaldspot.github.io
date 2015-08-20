b30f4c58e25b8c24d5b05fdfe84cca55b3154949
John Ptacek <scottwalkerbaldspot@gmail.com>
2015-08-20 18:11:24 -0500

    Add new SSAS data

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
+    sass = require('gulp-sass') 
     notify = require("gulp-notify") 
     bower = require('gulp-bower');
 
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
+    publicDir: "./public",
+    sassPath: "./resources/sass",
+     bowerDir: "./bower_components" 
   }
 
   gulp.task('bower', function() { 
@@ -15,25 +16,23 @@ var config = {
 
 // Move fontawesome files
 gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
+    return gulp.src(config.bowerDir + "/fontawesome/fonts/**.*") 
+        .pipe(gulp.dest("./public/fonts")); 
 });
 
-gulp.task('css', function() { 
+
+
+gulp.task('css', function() {
     return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-            .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+    .pipe(sass({
+        includePaths: [
+          "./resources/sass",
+                 config.bowerDir + "/bootstrap-sass-official/assets/stylesheets",
+                 config.bowerDir + "/fontawesome/scss"],
+    }))
+    .pipe(gulp.dest(config.publicDir + '/css'));
 });
+
 
 // Rerun the task when a file changes
  gulp.task('watch', function() {
