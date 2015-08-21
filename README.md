60aae3a2ef6f9271c9bd9343a1375e65b055846f
John Ptacek <scottwalkerbaldspot@gmail.com>
2015-08-20 22:05:45 -0500

    Add documents to github

--- README.md
+++
@@ -1,4 +0,0 @@
-# walkersbaldspot.github.io
-
-Chronicling the adventures of Scott Walker's Bald Spot as it fights for truth,
-justice and the American way
--- gulpfile.js
+++
@@ -1,42 +0,0 @@
-var gulp = require('gulp'), 
-    sass = require('gulp-sass') 
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
-
-var config = {
-    publicDir: "./public",
-    sassPath: "./resources/sass",
-     bowerDir: "./bower_components" 
-  }
-
-  gulp.task('bower', function() { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
-});
-
-// Move fontawesome files
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + "/fontawesome/fonts/**.*") 
-        .pipe(gulp.dest("./public/fonts")); 
-});
-
-
-
-gulp.task('css', function() {
-    return gulp.src(config.sassPath + '/style.scss')
-    .pipe(sass({
-        includePaths: [
-          "./resources/sass",
-                 config.bowerDir + "/bootstrap-sass-official/assets/stylesheets",
-                 config.bowerDir + "/fontawesome/scss"],
-    }))
-    .pipe(gulp.dest(config.publicDir + '/css'));
-});
-
-
-// Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
-});
-
-  gulp.task('default', ['bower', 'icons', 'css']);
